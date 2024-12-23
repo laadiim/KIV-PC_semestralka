061291b4a5905ab07ee200d7ef61fbe60296c37b
laadiim <cakora.ladislav@gmail.com>
2024-12-23 17:48:28 +0100

    input

--- main.c
+++ main.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int Check_if_limits(char *arg)
+{
+    int colons = 0;
+    while (*arg != '\0')
+    {
+        if (*arg == ':') colons++;
+        arg += sizeof(char);
+    }
+    return colons;
+}
+
 /* ENTRY POINT */
 int main(int argc, char *argv[])
 {
-	/* sanity check */
-	if (argc < 2)
-	{
-		printf("Not enough parameters!\n");
-		printf("Usage: %s <equation> <x_min>:<x_max>:<y_min>:<y_max>", argv[0]);
-		return 1;
-	}
-	return EXIT_SUCCESS;
+    int limits = 0;
+		char *out_name;
+
+    /* Sanity check */
+    if (argc < 2)
+    {
+        printf("Not enough parameters!\n");
+        printf("Usage: %s <equation> <output file> [<limits>]\n", argv[0]);
+        return 1;
+    }
+
+    limits = Check_if_limits(argv[argc - 1]);
+		if (limits != 0 && limits != 3)
+		{
+			printf("Invalid limits format!\n");
+      printf("Usage: <x_min>:<x_max>:<y_min>:<y_max>\n");
+			return 4;
+		}
+
+		out_name = argv[argc - 1 - limits / 3];
+
+    printf("limits: %d\n", limits);
+		printf("out file: %s\n", out_name);
+    return EXIT_SUCCESS;
 }
+
